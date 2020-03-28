647ee2dcd8200e65c1ba6819f7b684c23fb772b2
Vignesh Ramachandra <vickyramachandra@gmail.com>
2020-03-28 14:11:32 +0530

    Add NfcB to the list of supported tags

--- library/src/main/java/com/vignesh/library/NFCCardManager.java
+++ library/src/main/java/com/vignesh/library/NFCCardManager.java
@@ -7,17 +7,19 @@ import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.IsoDep;
 import android.nfc.tech.NfcA;
+import android.nfc.tech.NfcB;
 
 public class NFCCardManager {
 
   private final NfcAdapter mNfcAdapter;
   private final PendingIntent mPendingIntent;
   private final Activity mActivity;
+
   private static final IntentFilter[] INTENT_FILTER = new IntentFilter[] {
       new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED),
       new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)};
   private static final String[][] TECH_LIST = new String[][] { {
-      NfcA.class.getName(), IsoDep.class.getName() } };
+      NfcA.class.getName(), NfcB.class.getName(), IsoDep.class.getName() } };
 
   public NFCCardManager(final Activity pActivity) {
     mActivity = pActivity;
